598d7147bfbce1474a5395dca669d8454f248645
league <league@imac-12.attlocal.net>
2025-10-03 18:28:42 -0700

    trbrtbbtsdhfbsztrfvvdx hzrfyh

--- src/_01_algorithms/_3_goofy_names/GoofyNames.java
+++ src/_01_algorithms/_3_goofy_names/GoofyNames.java
@@ -11,29 +11,38 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+		String userName = JOptionPane.showInputDialog("enter ur name");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
-
+		System.out.println(userName.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 7).
 		//    HINT: Use .length() to determine the number of characters in the String.
-
-				// 4. Create a char variable to store the next character of the name
+		System.out.println(userName.length());
+			for(int i = 0; i < userName.length(); i++) {
+				
+				
+		// 4. Create a char variable to store the next character of the name
 				//    use .charAt()
-	
+				char letter = userName.charAt(i);
 				
 				// 5. Use MODULO operator (%) to identify if it is an EVEN or ODD character.
-			
+				if(i % 2 == 0) {
+					letter = Character.toUpperCase(letter);
+				}
+				else {
+					letter = Character.toLowerCase(letter);
+				}
 			
 				// 6. Even characters should be made uppercase and odd characters made lowercase
 				//    HINT: use Character.toUpperCase() or Character.toLowerCase()
 	
 			
 				// 7. ADD the char to the end of the goofyName String
-
+				goofyName+=letter;
+			}
 		
 		// 8. Use pop-up to show user their Goofy name
-
+			JOptionPane.showMessageDialog(null, goofyName);
 	}
 }
 
--- src/_02_nested_loops/_1_snowflake/Snowflake.java
+++ src/_02_nested_loops/_1_snowflake/Snowflake.java
@@ -2,22 +2,25 @@ package _02_nested_loops._1_snowflake;
 
 import java.awt.Color;
 
+import org.jointheleague.graphical.robot.Robot;
+
+
 
 public class Snowflake {
 	
 	public static void main(String[] args) {
 
 		// #1. Make a new Robot
-
+		Robot tim = new Robot();
 
 		// #2. Set your robot’s position to x=300 and y=300
-
-
+		tim.setX(300);
+		tim.setY(300);
 		// #3. Put the robot's pen down
-
+		tim.penDown();
 
 		// #4. Set the robot’s speed to 5
-
+		tim.setSpeed(5);
 
 		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6 times (use i as the counter)
 
